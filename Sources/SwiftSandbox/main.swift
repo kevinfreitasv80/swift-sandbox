da1575dc1407b8b51debbbeb1a932d8e2fad1e71
kevinfreitasv80 <kevinfreitasv80@gmail.com>
2026-08-18 17:45:45 -0300

    atividade de swift

--- Sources/SwiftSandbox/main.swift
+++ Sources/SwiftSandbox/main.swift
@@ -1,5 +1,75 @@
-print("Hello, World!")
+let cards = [1, 2, 3]
 
-let nome = "Kevin"
+/// Obtém a carta no índice especificado da pilha.
+///
+/// - Parameters:
+///   - index: O índice da carta que será obtida.
+///   - stack: A pilha que contém as cartas.
+/// - Returns: A carta localizada no índice especificado.
+func getCard(at index: Int, from stack: [Int]) -> Int {
+  return stack[index]
+}
 
-print("Seja bem-vindo(a): \(nome)")
+/// Substitui a carta existente no índice especificado por uma nova carta.
+///
+/// - Parameters:
+///   - index: O índice da carta que será substituída.
+///   - stack: A pilha que contém a carta a ser substituída.
+///   - newCard: A nova carta que substituirá a carta existente.
+/// - Returns: Uma nova pilha com a carta no índice especificado substituída por `newCard`.
+func setCard(at index: Int, in stack: [Int], to newCard: Int) -> [Int] {
+    var mutableStack = stack
+    mutableStack[index] = newCard
+    
+    return mutableStack
+}
+
+/// Insere uma nova carta no topo da pilha.
+///
+/// - Parameters:
+///   - newCard: A carta que será inserida.
+///   - stack: A pilha onde a carta será inserida.
+/// - Returns: Uma nova pilha com a carta inserida no topo.
+func insert(_ newCard: Int, atTopOf stack: [Int]) -> [Int] {
+    var mutableStack = stack
+    mutableStack.insert(newCard, at: 0)
+    
+    return mutableStack
+}
+
+/// Remove a carta no índice especificado da pilha.
+///
+/// - Parameters:
+///   - index: O índice da carta que será removida.
+///   - stack: A pilha que contém as cartas.
+/// - Returns: Uma nova pilha sem a carta removida.
+func removeCard(at index: Int, from stack: [Int]) -> [Int] {
+    var mutableStack = stack
+    mutableStack.remove(at: index)
+    
+    return mutableStack
+}
+
+/// Insere uma nova carta no índice especificado da pilha.
+///
+/// - Parameters:
+///   - newCard: A carta que será inserida.
+///   - index: O índice onde a carta será inserida.
+///   - stack: A pilha que contém as cartas.
+/// - Returns: Uma nova pilha com a carta inserida no índice especificado.
+func insert(_ newCard: Int, at index: Int, from stack: [Int]) -> [Int] {
+    var mutableStack = stack
+    mutableStack.insert(newCard, at: index)
+    
+    return mutableStack
+}
+
+/// Verifica se a pilha possui a quantidade especificada de cartas.
+///
+/// - Parameters:
+///   - stack: A pilha que será verificada.
+///   - size: A quantidade esperada de cartas.
+/// - Returns: `true` se a pilha tiver a quantidade especificada de cartas; caso contrário, `false`.
+func checkSizeOfStack(_ stack: [Int], _ size: Int) -> Bool {
+    return stack.count == size
+}
